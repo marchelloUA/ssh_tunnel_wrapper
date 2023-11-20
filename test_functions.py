23ad150359271035931f012d90d8e88206aa8853
marchelloUA <marchelloUA@users.noreply.github.com>
2023-11-20 23:23:29 +0100

    Create test_functions.py
    added sample test for functions

---
+++ test_functions.py
@@ -0,0 +1,7 @@
+from app import is_positive_integer
+
+def test_is_positive_integer():
+    assert is_positive_integer('10') == True
+    assert is_positive_integer('-5') == False
+    assert is_positive_integer('0') == False
+    assert is_positive_integer('abc') == False
